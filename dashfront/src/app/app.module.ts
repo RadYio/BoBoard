174b14060f4491af8db7ee1700516f987f7155f5
Hadamar en Personne <allan.gonin-saget.etu@univ-lemans.fr>
2023-02-02 19:30:39 +0100

    mise a jour de bg

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './components/meteo/meteo.component';
 import { CouisineComponent } from './components/couisine/couisine.component';
+import { Meteo2Component } from './components/meteo2/meteo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeteoComponent,
     CouisineComponent,
+    Meteo2Component,
   ],
   imports: [
     BrowserModule
