79e4d7c5d90564efeafacff79741f01af3461a1f
ArthurBlr <art.boullier@gmail.com>
2023-02-02 17:56:24 +0100

    couisine

--- dashfront/src/app/app.module.ts
+++ dashfront/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './components/meteo/meteo.component';
+import { CouisineComponent } from './components/couisine/couisine.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeteoComponent,
+    CouisineComponent,
   ],
   imports: [
     BrowserModule
---
+++ dashfront/src/app/components/couisine/couisine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-couisine',
+  templateUrl: './couisine.component.html',
+  styleUrls: ['./couisine.component.css']
+})
+export class CouisineComponent {
+
+}
