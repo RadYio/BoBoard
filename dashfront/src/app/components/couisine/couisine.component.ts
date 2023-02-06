a701ee6f8eb6276649fb7cd386e827533914ec34
ArthurBlr <art.boullier@gmail.com>
2023-02-06 16:05:00 +0100

    ca marche pas

--- dashfront/src/app/components/couisine/couisine.component.ts
+++ dashfront/src/app/components/couisine/couisine.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./couisine.component.css']
 })
 export class CouisineComponent {
-  recette1: String = "https://www.750g.com/pizza-6-fromages-r3414.htm";
+  recette1: String;
+
+
+  constructor(){
+    this.recette1 = "fesse";
+  };
+
+  ngOnInit(): void {
+
+    const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'b6dc3bdd08msh2a7c44ee7be54f1p10a5bajsne0816708109e',
+        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+      }
+    };
+    let API_KEY = 'b6dc3bdd08msh2a7c44ee7be54f1p10a5bajsne0816708109e';
+    fetch(url, options)
+      .then(res => res.json())
+      .then((data) => {
+        //Pour le nom
+        this.recette1 = data.results[0].name
+
+        alert(JSON.stringify(data.results[0].sections[0].components[0].ingredient.name));
+      });
+ 
+    
+  }
+  
 }
+
