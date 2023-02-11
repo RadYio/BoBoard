b5009376fab22b8b7d7e86246bd780981a942a3c
jck0day <jck0day@gmail.com>
2023-02-11 13:04:35 +0100

    Ajout des news

--- dashfront/front/src/app/components/news/news.component.ts
+++ dashfront/front/src/app/components/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { News } from './news';
 
 @Component({
   selector: 'app-news',
@@ -7,19 +8,29 @@ import { Component } from '@angular/core';
 })
 
 export class NewsComponent {
-  titre: String = "ta mere";
+  news: News[] = [];
 
   constructor(){
     const options = {
       method: 'GET'
     };
 
-    fetch('http://api.mediastack.com/v1/news?access_key=&countries=fr', options) //8d510f151fea9c5e7b31f8fba58e4912
-      .then(response => response.json())
-      
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+    let apiKey = "null"; //8d510f151fea9c5e7b31f8fba58e4912
 
+    fetch('http://api.mediastack.com/v1/news?access_key='+apiKey+'&countries=fr', options)
+      .then(response => response.json())
+      .then(response => {
+
+        for(let i = 0; i < 10; i++){
+          this.news[i]=new News();
+            this.news[i].titre = response.data[i].title;
+            this.news[i].author = response.data[i].author;
+            this.news[i].description = response.data[i].description;
+            this.news[i].url = response.data[i].url;
+            this.news[i].imageUrl = response.data[i].image;
+            this.news[i].date = response.data[i].published_at;
+          }
+        })
   }
 }
 
---
+++ dashfront/front/src/app/components/news/news.ts
@@ -0,0 +1,9 @@
+
+export class News {
+    titre: String[] = ["Titre"];
+    author: String = "Auteur";
+    description: String = "Description";
+    url: String = "none";
+    imageUrl: String = "none";
+    date: String = "00 - 00 - 00";
+  }
