d4e23f8c071e1ad540ae77e24747895287b3ec72
xHookman <xhookmandev@gmail.com>
2023-03-07 19:18:12 +0100

    Ajout d'un bouton pour se géolocaliser sur Maps

--- dashfront/front/src/app/components/maps/maps.component.ts
+++ dashfront/front/src/app/components/maps/maps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 
+let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
+
 const loader = new Loader({
   apiKey: "AIzaSyBUnyVyvNnyU_7GoqJvZfMhw4rsBjZ0Fhc",
   version: "weekly",
@@ -15,20 +17,69 @@ let mapOptions = {
   zoom: 4
 };
 
-function initMap(lat, lng){
-  mapOptions.center.lat=lat;
-  mapOptions.center.lng=lng;
-
+function initMap(){
   // Promise
-loader.load()
-.then((google) => {
-  new google.maps.Map(document.getElementById("map"), mapOptions);
-})
-.catch(e => {
-  // do something
-});
+loader.load().then((google) => {
+  map = new google.maps.Map(document.getElementById("map"), mapOptions); // Creer la map
+
+
+  infoWindow = new google.maps.InfoWindow(); // Petite card pour afficher une erreur dans la map si la localisation pose problème
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Géolocaliser";
+  locationButton.classList.add("custom-map-control-button");
+
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter()!);
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter()!);
+    }
+  });
+  })
 }
 
+function handleLocationError(
+  browserHasGeolocation: boolean,
+  infoWindow: google.maps.InfoWindow,
+  pos: google.maps.LatLng
+) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
 
 @Component({
   selector: 'app-maps',
@@ -37,8 +88,7 @@ loader.load()
 })
 
 export class MapsComponent implements OnInit {
-
   ngOnInit(): void {
-    initMap(40, 40);
+    initMap();
   }
 }
