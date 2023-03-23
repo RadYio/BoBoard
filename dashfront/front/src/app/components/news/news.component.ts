ebfbee6b6f62c4162dd23d6e856670ba086199d7
jck0day <jck0day@gmail.com>
2023-03-23 18:11:58 +0100

    Ptit fix visuel pour les news

--- dashfront/front/src/app/components/news/news.component.ts
+++ dashfront/front/src/app/components/news/news.component.ts
@@ -19,34 +19,18 @@ export class NewsComponent implements OnInit  {
       method: 'GET'
     };
 
-      fetch("https://fesse.onrender.com/news")
-      .then((response) => response.json())
-        .then((response) => {
-          for(let i = 0; i < 10; i++){
-              this.news[i]=new News();
-              this.news[i].titre = response.data[i].title;
-              this.news[i].author = response.data[i].author;
-              this.news[i].description = response.data[i].description;
-              this.news[i].url = response.data[i].url;
-              this.news[i].imageUrl = response.data[i].image;
-              this.news[i].date = response.data[i].published_at;
-            }
-        });
-
-
-   /* fetch('http://api.mediastack.com/v1/news?access_key='+apiKey+'&countries=fr', options)
-      .then(response => response.json())
-      .then(response => {
-
-        for(let i = 0; i < 10; i++){
+  fetch("https://fesse.onrender.com/news")
+  .then((response) => response.json())
+    .then((response) => {
+      for(let i = 0; i < 9; i++){
           this.news[i]=new News();
-            this.news[i].titre = response.data[i].title;
-            this.news[i].author = response.data[i].author;
-            this.news[i].description = response.data[i].description;
-            this.news[i].url = response.data[i].url;
-            this.news[i].imageUrl = response.data[i].image;
-            this.news[i].date = response.data[i].published_at;
-          }
-        });*/
-      }
+          this.news[i].titre = response.data[i].title;
+          this.news[i].author = response.data[i].author;
+          this.news[i].description = response.data[i].description;
+          this.news[i].url = response.data[i].url;
+          this.news[i].imageUrl = response.data[i].image;
+          this.news[i].date = response.data[i].published_at;
+        }
+    });
+  }
 }
